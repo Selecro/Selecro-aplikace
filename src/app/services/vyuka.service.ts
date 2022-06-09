6f207891464d7e13b91f44d52a8c8c5f64861c5b
Václav Gryc <Vaclav.Gryc@ibm.com>
2022-06-09 12:50:16 +0200

    upravy vyukovy mod

--- src/app/services/vyuka.service.ts
+++ src/app/services/vyuka.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Navod,Postup} from '../types'
+import {Navod} from '../types'
 
 const poleNavodu: Array<Navod>= [
   {nazev: "Chobotnicka",
@@ -11,7 +11,27 @@ const poleNavodu: Array<Navod>= [
      videoUrl: "dBmU8n4AfDg"
    }
  ]
+ },
+ {nazev: "Chobotnicka",
+ popis: "Navod na vytvoreni chobotnicky",
+ kroky:[{
+   nazev: '1. krok',
+   pouzijeme: "Mk",
+   popis:" Nejdůležitější na tomto kroku je neutahovat, abyste byli schopni konečný výsledek utáhnout. Neustále si hlídám zadní nit, aby mi neutekla. Potřebujeme ji tam mít zaháčkovanou, abychom měli vůbec možnost utáhnout celý kroužek.</p><p>Postup na magický kroužek je uveden ve slovníku v jeho samostatné kapitole. Existuje však spousta způsobů, jak jej vytvořit. Ve většině způsobů se dostanete ke stejnému základu, jako ve videu (viz 35 vteřina). Můžeme k tomu však použít i pouhá 2 řetízková oka, vrátit se a udělat do toho prvního 6 ks.",
+   videoUrl: "dBmU8n4AfDg"
  }
+]
+},
+{nazev: "Chobotnicka",
+popis: "Navod na vytvoreni chobotnicky",
+kroky:[{
+  nazev: '1. krok',
+  pouzijeme: "Mk",
+  popis:" Nejdůležitější na tomto kroku je neutahovat, abyste byli schopni konečný výsledek utáhnout. Neustále si hlídám zadní nit, aby mi neutekla. Potřebujeme ji tam mít zaháčkovanou, abychom měli vůbec možnost utáhnout celý kroužek.</p><p>Postup na magický kroužek je uveden ve slovníku v jeho samostatné kapitole. Existuje však spousta způsobů, jak jej vytvořit. Ve většině způsobů se dostanete ke stejnému základu, jako ve videu (viz 35 vteřina). Můžeme k tomu však použít i pouhá 2 řetízková oka, vrátit se a udělat do toho prvního 6 ks.",
+  videoUrl: "dBmU8n4AfDg"
+}
+]
+}
 ]
  @Injectable({
   providedIn: 'root'
