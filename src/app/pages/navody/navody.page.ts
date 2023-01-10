9aba334ab75931e774ee08325e7a7825ccb29847
Szotkowski <mikiszotkowski@gmail.com>
2023-01-10 23:09:39 +0100

    input text is in console

--- src/app/pages/navody/navody.page.ts
+++ src/app/pages/navody/navody.page.ts
@@ -14,13 +14,15 @@ export class NavodyPage implements OnInit {
   items: Array<Navod>;
   obtiznost: String;
   selectedElement: String;
+  searchInput: any;
 
   constructor(private router: Router, private navodyService: NavodyService) { 
-    //this.obtiznost = "lehky";
   }
 
   ngOnInit() {
     this.items = this.navodyService.getVsechnyNavody();
+    this.searchInput = document.querySelector("input");
+    this.inputbox();
     //this.obtiznost = "lehky";
   }
 
@@ -49,35 +51,28 @@ export class NavodyPage implements OnInit {
   dokocene(event: Event) {
     console.log("ble")
   }
-
-  public search() {
+  
+  inputbox() {
     const searchBox = document.querySelector(".search-box");
     const searchBtn = document.querySelector(".search-icon");
     const cancelBtn = document.querySelector(".cancel-icon");
-    const searchInput = document.querySelector("input");
-    const searchData = document.querySelector(".search-data");
     document.getElementById("search-icon").onclick =()=>{
       searchBox.classList.add("active");
       searchBtn.classList.add("active");
-      searchInput.classList.add("active");
+      this.searchInput.classList.add("active");
       cancelBtn.classList.add("active");
-      searchInput.focus();
-      if(searchInput.value != "") {
-        var values = searchInput.value;
-        searchData.classList.remove("active");
-        searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
-      }
-      else{
-        searchData.textContent = "";
-      }
+      this.searchInput.focus();
     }
     document.getElementById("cancel-icon").onclick =()=>{
       searchBox.classList.remove("active");
       searchBtn.classList.remove("active");
-      searchInput.classList.remove("active");
+      this.searchInput.classList.remove("active");
       cancelBtn.classList.remove("active");
-      searchData.classList.toggle("active");
-      searchInput.value = "";
+      this.searchInput.value = "";
     }
+  }
+
+  search() {
+    console.log(this.searchInput.value);
   }
 } 
--- src/app/pages/vyukovy-mod/vyukovy-mod.page.ts
+++ src/app/pages/vyukovy-mod/vyukovy-mod.page.ts
@@ -11,46 +11,42 @@ import { Vyrobek } from '../../types';
 export class VyukovyModPage implements OnInit {
 
   items: Array<Vyrobek>;
+  searchInput: any;
 
   constructor(private router: Router, private vyukaService: VyukaService) {
    }
 
   ngOnInit() {
-    this.items = this.vyukaService.getVsechnyVyrobky()
+    this.items = this.vyukaService.getVsechnyVyrobky();
+    this.searchInput = document.querySelector("input");
+    this.inputbox();
   }
 
   goDetail(item: Vyrobek) {
     this.router.navigate([`vyukovy-mod/detail`, {nazevVyrobku: item.nazev}]);
   }
 
-  public search() {
+  inputbox() {
     const searchBox = document.querySelector(".search-box");
     const searchBtn = document.querySelector(".search-icon");
     const cancelBtn = document.querySelector(".cancel-icon");
-    const searchInput = document.querySelector("input");
-    const searchData = document.querySelector(".search-data");
     document.getElementById("search-icon").onclick =()=>{
       searchBox.classList.add("active");
       searchBtn.classList.add("active");
-      searchInput.classList.add("active");
+      this.searchInput.classList.add("active");
       cancelBtn.classList.add("active");
-      searchInput.focus();
-      if(searchInput.value != "") {
-        var values = searchInput.value;
-        searchData.classList.remove("active");
-        searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
-      }
-      else{
-        searchData.textContent = "";
-      }
+      this.searchInput.focus();
     }
     document.getElementById("cancel-icon").onclick =()=>{
       searchBox.classList.remove("active");
       searchBtn.classList.remove("active");
-      searchInput.classList.remove("active");
+      this.searchInput.classList.remove("active");
       cancelBtn.classList.remove("active");
-      searchData.classList.toggle("active");
-      searchInput.value = "";
+      this.searchInput.value = "";
     }
+  }
+
+  search() {
+    console.log(this.searchInput.value);
   }
 }  
