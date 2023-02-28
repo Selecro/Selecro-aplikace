94a5f9330714d9ac277ade141e30ee64a1cc6671
Aneta Wernerová <aneta.werner@seznam.cz>
2023-02-28 15:45:31 +0100

    nefunguje

--- src/app/pages/prihlaseni/prihlaseni.page.ts
+++ src/app/pages/prihlaseni/prihlaseni.page.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prihlaseni.page.scss'],
 })
 export class PrihlaseniPage implements OnInit {
+  uzivatel: HTMLElement;
+  heslo: HTMLElement;
+  
+  public showpassword: boolean
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
+    
   }
 
+  public userName() {
+    let sampleRegEx = new RegExp('^[a-zA-Z0-9_.-]{4,20}$');
+    console.log(sampleRegEx.test("this.uzivatel"))
+  }
+
+  public password() {
+    let sampleRegEx = new RegExp('^[a-zA-Z0-9?!.,_-]{8,20}$');
+    console.log(sampleRegEx.test("this.prnviHeslo"));
+    console.log(sampleRegEx.test("this.druheHeslo"))
+  }
+
+  public hideShowPassword() {
+    this.showpassword = !this.showpassword;
+  }
 }
--- src/app/pages/prihlaseni/registrace/registrace.page.ts
+++ src/app/pages/prihlaseni/registrace/registrace.page.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrace.page.scss'],
 })
 export class RegistracePage implements OnInit {
+  uzivatel: HTMLElement;
+  prvniHeslo: HTMLElement;
+  druheHeslo: HTMLElement;
+  adresa: HTMLElement;
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+ 
 
-  constructor() { }
+  constructor() {
+    this.uzivatel = document.getElementById("user");
+    this.prvniHeslo = document.getElementById("password");
+    this.druheHeslo = document.getElementById("secondPassword");
+    this.adresa = document.getElementById("email");
+    
+  }
 
   ngOnInit() {
   }
 
+  public userName() {
+    let sampleRegEx = new RegExp('^[a-zA-Z0-9_.-]{4,20}$');
+    console.log(sampleRegEx.test("this.uzivatel"))
+  }
+
+  public password() {
+    let sampleRegEx = new RegExp('^[a-zA-Z0-9?!.,_-]{8,20}$');
+    console.log(sampleRegEx.test("this.prnviHeslo"));
+    console.log(sampleRegEx.test("this.druheHeslo"))
+  }
+
+  public email() {
+    let sampleRegEx = new RegExp('[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}');
+    console.log(sampleRegEx.test("this.adresa"))
+  }
+
+  public showPassword(input: any): any {
+    input.type = input.type === 'password' ?  'text' : 'password';
+   }
+
+   hideShowPassword() {
+       this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+       this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+   }
 }
