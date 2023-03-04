c64fcfa31b8d027af8d512fdc2f919e6c713549d
Szotkowski <mikiszotkowski@protonmail.com>
2023-03-04 20:58:51 +0100

    functional registration

--- src/app/pages/prihlaseni/registrace/registrace.page.ts
+++ src/app/pages/prihlaseni/registrace/registrace.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import axios from 'axios';
+
+enum Language {
+  CZ = 'CZ',
+  EN = 'EN'
+}
 
 @Component({
   selector: 'app-registrace',
@@ -6,44 +14,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrace.page.scss'],
 })
 export class RegistracePage implements OnInit {
-  uzivatel: HTMLElement;
-  prvniHeslo: HTMLElement;
-  druheHeslo: HTMLElement;
-  adresa: HTMLElement;
-  showPassword = false;
-  showPassword2 = false;
   passwordToggleIcon = "eye";
   passwordToggleIcon2 = "eye";
-  
-  constructor() {}
+  showPassword0: boolean;
+  showPassword1: boolean;
+  email: string;
+  password0: string;
+  password1: string;
+  language: Language;
+  username: string;
+  usernameRegEx = new RegExp('^[a-zA-Z0-9_.-]{4,20}$');
+  emailRegEx = new RegExp('[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}');
+  passwordRegEx = new RegExp('^[a-zA-Z0-9?!.,_-]{8,20}$');
+
+  constructor(private router: Router, private loadingController: LoadingController) { }
 
   ngOnInit() {
-    this.uzivatel = document.getElementById("user");
-    this.prvniHeslo = document.getElementById("password");
-    this.druheHeslo = document.getElementById("secondPassword");
-    this.adresa = document.getElementById("email");
   }
 
-  public userName() {
-    let sampleRegEx = new RegExp('^[a-zA-Z0-9_.-]{4,20}$');
-    console.log(sampleRegEx.test("this.uzivatel"))
+  public usernameCheck(): boolean {
+    return this.usernameRegEx.test(this.username);
   }
 
-  public password() {
-    let sampleRegEx = new RegExp('^[a-zA-Z0-9?!.,_-]{8,20}$');
-    console.log(sampleRegEx.test("this.prnviHeslo"));
-    console.log(sampleRegEx.test("this.druheHeslo"))
+  public emailCheck(): boolean {
+    return this.emailRegEx.test(this.email);
   }
 
-  public email() {
-    let sampleRegEx = new RegExp('[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}');
-    console.log(sampleRegEx.test("this.adresa"))
+  public passwordCheck(): boolean {
+    if ((this.password0 == this.password1) && (this.passwordRegEx.test(this.password0) == true) && (this.passwordRegEx.test(this.password1) == true)) {
+      return true;
+    }
+    return false;
   }
 
-  togglePassword(): void {
-    this.showPassword = !this.showPassword;
-
-    if(this.passwordToggleIcon == 'eye') {
+  public togglePassword() {
+    this.showPassword0 = !this.showPassword0;
+    if (this.passwordToggleIcon == 'eye') {
       this.passwordToggleIcon = 'eye-off';
     }
     else {
@@ -51,14 +57,39 @@ export class RegistracePage implements OnInit {
     }
   }
 
-  togglePassword2(): void {
-    this.showPassword2 = !this.showPassword2;
-
-    if(this.passwordToggleIcon2 == 'eye') {
+  public togglePassword2() {
+    this.showPassword1 = !this.showPassword1;
+    if (this.passwordToggleIcon2 == 'eye') {
       this.passwordToggleIcon2 = 'eye-off';
     }
     else {
       this.passwordToggleIcon2 = 'eye';
+    }
+  }
+
+  public async register() {
+    if ((this.usernameCheck() == true) && (this.emailCheck() == true) && (this.passwordCheck() == true)) {
+      const body = {
+        email: this.email,
+        password: this.password0,
+        username: this.username,
+        language: this.language,
+      };
+      const loading = await this.loadingController.create({
+        message: 'Please wait...',
+      });
+      try {
+        await loading.present();
+        axios.post('http://192.168.0.4:3002/signup', body).then(response => {
+          this.router.navigateByUrl('/prihlaseni');
+        }).catch(error => {
+          console.error(error);
+        });
+        await loading.dismiss();
+      }
+      catch(error) {
+        console.error(error);
+      }
     }
   }
 }
